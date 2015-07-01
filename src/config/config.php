174dac79ce6ef7376e1931e894690223330a8870
hacpaka <hacpaka@eggbe.com>
2015-07-01 16:53:07 +0300

    +

--- src/config/config.php
+++ src/config/config.php
@@ -1,10 +1,21 @@
 <?php
 use \Eggbe\DelegapiServer\Server;
 
+/*
+|--------------------------------------------------------------------------
+| Example
+|--------------------------------------------------------------------------
 return [
 	'listen' => [
-		Server::LISTEN_SECURE => 'SecurityListener',
+		Server::LISTEN_SECURE => 'SecureListener',
 		Server::LISTEN_ACTION => 'ActionListener',
 		Server::LISTEN_SESSION => 'SessionListener',
 	],
+];
+*/
+
+return [
+	'listen' => [
+
+	]
 ];
