82291e40c6f50edd6c933ea8cd9700e18a3fc3cb
hacpaka <hacpaka@eggbe.com>
2016-07-26 20:36:55 +0300

    Config file simplification

--- config/delegapi-server.php
+++ config/delegapi-server.php
@@ -2,18 +2,16 @@
 use \Eggbe\DelegapiServer\Server;
 
 return [
-	'listen' => [
 
-		/**
-		 * Binding the simple callback function to the action.
-		 */
-		Server::ON_AUTHORIZE => function(){},
+	/**
+	 * Binding the simple callback function to the action.
+	 */
+	Server::ON_AUTHORIZE => function(){},
 
-		/**
-		 * Binding the special listener class to the action.
-		 * The listener have to extend Eggbe\DelegapiServer\Abstracts\AListener.
-		 */
-		Server::ON_EXECUTE => 'ActionListener',
+	/**
+	 * Binding the special listener class to the action.
+	 * The listener have to extend Eggbe\DelegapiServer\Abstracts\AListener.
+	 */
+	Server::ON_EXECUTE => 'ActionListener',
 
-	],
 ];
